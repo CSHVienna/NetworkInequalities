0f31ef8535f7acce38a7a1125e4bde8b6257844b
Lisette Espin <leespinn@gmail.com>
2023-04-25 19:57:25 +0200

    update version

--- netin/__init__.py
+++ netin/__init__.py
@@ -4,7 +4,7 @@ It is based on the NetworkX package and provides a set of functions to study ine
 in social networks.
 """
 
-__version__ = '1.0.5.8.2'
+__version__ = '1.0.5.8.3'
 
 from netin import utils
 from netin.utils import *
