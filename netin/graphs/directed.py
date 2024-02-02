0f715b3f6b0502191eb1d98fadc6b6be2b04e9a6
Jan Bachmann <jan@mannbach.de>
2024-02-02 17:47:53 +0100

    Remove leftover imports

--- netin/graphs/directed.py
+++ netin/graphs/directed.py
@@ -1,14 +1,6 @@
-from collections import defaultdict
-from typing import Union
-
 import networkx as nx
-import numpy as np
-import powerlaw
 
-from netin.utils import constants as const
-from netin.utils import validator as val
 from .graph import Graph
-
 
 class DiGraph(Graph, nx.DiGraph):
     """Directed graph."""
