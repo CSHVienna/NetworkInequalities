2df8668d58e3948e2483779f0c5b825697b2c90b
Jan Bachmann <jan@mannbach.de>
2024-02-02 12:47:05 +0100

    Documentation

--- netin/link_formation_mechanisms/preferential_attachment.py
+++ netin/link_formation_mechanisms/preferential_attachment.py
@@ -5,9 +5,26 @@ from netin.graphs.event import Event
 from .link_formation_mechanism import LinkFormationMechanism
 
 class PreferentialAttachment(LinkFormationMechanism):
+    """
+    A class representing the preferential attachment link formation mechanism.
+
+    Parameters
+    ----------
+    LinkFormationMechanism : type
+        The base class for link formation mechanisms.
+    """
     _a_degree: np.ndarray
 
     def __init__(self, graph: Graph) -> None:
+        """
+        Initializes a PreferentialAttachment object.
+
+        Args:
+            graph (Graph): The graph object representing the network.
+
+        Returns:
+            None
+        """
         super().__init__(graph)
         self._a_degree = np.zeros(len(graph), dtype=int)
         for i,k in graph.degree():
@@ -17,8 +34,27 @@ class PreferentialAttachment(LinkFormationMechanism):
             function=self._update_degree_by_link)
 
     def get_target_probabilities(self, _) -> np.ndarray:
+        """
+        Calculates the target probabilities for link formation based on the preferential attachment mechanism.
+
+        Args:
+            _ : Placeholder argument.
+
+        Returns:
+            np.ndarray: An array of target probabilities for each node in the network.
+        """
         return self._a_degree / np.sum(self._a_degree)
 
     def _update_degree_by_link(self, source: int, target: int):
+        """
+        Updates the degree of nodes when a new link is added.
+
+        Args:
+            source (int): The source node of the new link.
+            target (int): The target node of the new link.
+
+        Returns:
+            None
+        """
         self._a_degree[source] += 1
         self._a_degree[target] += 1
--- netin/link_formation_mechanisms/triadic_closure.py
+++ netin/link_formation_mechanisms/triadic_closure.py
@@ -5,6 +5,23 @@ from netin.graphs.event import Event
 from .link_formation_mechanism import LinkFormationMechanism
 
 class TriadicClosure(LinkFormationMechanism):
+    """
+    A link formation mechanism based on triadic closure.
+
+    This mechanism calculates the probabilities of forming links based on the concept of triadic closure,
+    which states that individuals who have a common friend are more likely to form a connection.
+
+    Attributes:
+        _a_friend_of_friends (np.ndarray): Array representing the presence of friends of friends.
+        _source_curr (int): Current source node.
+
+    Methods:
+        __init__(self, graph: Graph): Initializes the TriadicClosure object.
+        _init_friends_of_friends(self, source: int): Initializes the array of friends of friends.
+        _update_friends_of_friends(self, source: int, target: int): Updates the array of friends of friends.
+        get_target_probabilities(self, source): Returns the probabilities of forming links to target nodes.
+    """
+
     _a_friend_of_friends: np.ndarray
     _source_curr: int
 
@@ -16,6 +33,15 @@ class TriadicClosure(LinkFormationMechanism):
             function=self._update_friends_of_friends)
 
     def _init_friends_of_friends(self, source: int):
+        """
+        Initializes the array of friends of friends.
+
+        Args:
+            source (int): The source node.
+
+        Returns:
+            None
+        """
         self._source_curr = source
 
         self._a_friend_of_friends = np.zeros(len(self.graph))
@@ -28,6 +54,16 @@ class TriadicClosure(LinkFormationMechanism):
         self._a_friend_of_friends[fof] = 1.
 
     def _update_friends_of_friends(self, source: int, target: int):
+        """
+        Updates the array of friends of friends after a link was formed between `source` and `target`.
+
+        Args:
+            source (int): The source node.
+            target (int): The target node.
+
+        Returns:
+            None
+        """
         if source != self._source_curr:
             return
         for fof in self.graph.neighbors[target]:
@@ -35,6 +71,17 @@ class TriadicClosure(LinkFormationMechanism):
                 self._a_friend_of_friends[fof] = 1.
 
     def get_target_probabilities(self, source) -> np.ndarray:
+        """
+        Returns the probabilities of forming links to target nodes.
+
+        Target probabilities are one for nodes that are friends of friends of the source node and zero otherwise.
+
+        Args:
+            source: The source node.
+
+        Returns:
+            np.ndarray: Array representing the probabilities of forming links to target nodes.
+        """
         if source != self._source_curr:
             self._init_friends_of_friends(source=source)
         return self._a_friend_of_friends
