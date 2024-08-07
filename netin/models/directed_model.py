051c5ecae76a3736ee0a5649bc987d74ba8e91d7
Jan Bachmann <jan@mannbach.de>
2024-08-07 12:58:16 +0200

    First draft for directed models

--- netin/models/directed_model.py
+++ netin/models/directed_model.py
@@ -2,12 +2,15 @@ from typing import Dict, Optional, Any
 
 import powerlaw
 import numpy as np
+import networkx as nx
 
 from netin.graphs.graph import Graph
 
 from ..graphs.directed import DiGraph
 from ..graphs.node_attributes import NodeAttributes
+from ..graphs.event import Event
 from .model import Model
+from ..utils import constants as const
 
 class DirectedModel(Model):
     node_activity: NodeAttributes
@@ -25,12 +28,57 @@ class DirectedModel(Model):
         self.d = d
         self.plo_M = plo_M
         self.plo_m = plo_m
+        self._in_degrees = NodeAttributes(N, dtype=int, name="in_degrees")
+        self._out_degrees = NodeAttributes(N, dtype=int, name="out_degrees")
 
         super().__init__(N, f, graph, seed)
         self._initialize_node_activity()
 
     def _initialize_graph(self):
         self.graph = DiGraph()
+        self.graph.register_event_handler(
+            event=Event.LINK_ADD_AFTER, function=self._update_node_degrees
+        )
+
+    def _populate_initial_graph(self):
+        self.graph.add_nodes_from(range(self.N))
+
+    def _get_expected_number_of_edges(self):
+        return int(round(self.d * self.N * (self.N - 1)))
+
+    def _get_sources(self):
+        return np.random.choice(
+            a=np.arange(self.N),
+            size=self._get_expected_number_of_edges(),
+            replace=True,
+            p=self.node_activity)
+
+    def _get_target(self, source: int):
+        a_out_degrees = self._out_degrees.attr()
+
+        # Initialize uniform probabilities
+        # @TODO: Model these as link formation mechanisms
+        target_probabilities = np.full(self.N, 1/self.N)
+        target_probabilities[source] = 0. # Exclude self selection
+        target_probabilities[self.graph[source]] = 0. # Exclude existing links
+        one_percent = self.N * 1 / 100.
+        if np.count_nonzero(a_out_degrees) > one_percent:
+            # if there are enough edges, then select only nodes with out_degree > 0 that are not already
+            # connected to the source.
+            # Having out_degree > 0 means they are nodes that have been in the network for at least one time step
+            target_probabilities *= np.where(a_out_degrees > 0, 1., 0.)
+
+        # Check if all probabilities are zero
+        # Follows https://stackoverflow.com/questions/18395725/test-if-numpy-array-contains-only-zeros
+        if not np.any(target_probabilities):
+            return None
+
+        # Add potential link formation mechanisms
+        target_probabilities = self.compute_target_probabilities(source)
+
+        return self._sample_target_node(
+            target_probabilities=target_probabilities
+        )
 
     def _initialize_node_activity(self):
         act_M = powerlaw.Power_Law(
@@ -56,6 +104,10 @@ class DirectedModel(Model):
 
         self.node_activity = NodeAttributes.from_ndarray(a_node_activity, name="node_activity")
 
+    def _update_node_degrees(self, source: int, target: int):
+        self._in_degrees[target] += 1
+        self._out_degrees[source] += 1
+
     def get_metadata(
             self, d_meta_data: Optional[Dict[str, Any]] = None)\
                 -> Dict[str, Any]:
@@ -65,7 +117,29 @@ class DirectedModel(Model):
             "plo_M": self.plo_M,
             "plo_m": self.plo_m
         }
+        self._in_degrees.get_metadata(d[self.__class__.__name__])
+        self._out_degrees.get_metadata(d[self.__class__.__name__])
         return d
 
     def simulate(self) -> Graph:
-        raise NotImplementedError
+        tries = 0
+        while self.graph.number_of_edges() < self._get_expected_number_of_edges():
+            tries += 1
+            for source in self._get_sources():
+                target = self._get_target(source)
+
+                if target is None:
+                    continue
+
+                if self.graph.number_of_edges() >= self._get_expected_number_of_edges():
+                    break
+
+            # if no more edges can be added, break
+            if tries > const.MAX_TRIES_EDGE\
+                and (self.graph.number_of_edges() <\
+                     self._get_expected_number_of_edges()):
+                print((
+                    f">> Edge density ({nx.density(self)}) "
+                    f"might differ from {self.d:.5f} (N={self.N}, f={self.f}"
+                    f"seed={self.seed}, plo_M={self.plo_M}, plo_m={self.plo_m}"))
+                break
