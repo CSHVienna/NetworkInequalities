777e5dba4adeb55d2c955e0277292d94f3bb9f64
Jan Bachmann <jan@mannbach.de>
2024-08-07 15:58:46 +0200

    Fix order of inheritance

--- netin/graphs/directed.py
+++ netin/graphs/directed.py
@@ -4,3 +4,6 @@ from .graph import Graph
 
 class DiGraph(nx.DiGraph, Graph):
     """Directed graph."""
+    def __init__(self, incoming_graph_data=None, **attr):
+        nx.DiGraph.__init__(self, incoming_graph_data, **attr)
+        Graph.__init__(self)
