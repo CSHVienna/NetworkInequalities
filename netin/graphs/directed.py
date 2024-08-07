4a01590ab1a6fabd33cbdc044e313b982ab96239
Jan Bachmann <jan@mannbach.de>
2024-08-07 18:18:41 +0200

    Fix instructor call order

--- netin/graphs/directed.py
+++ netin/graphs/directed.py
@@ -5,5 +5,5 @@ from .graph import Graph
 class DiGraph(nx.DiGraph, Graph):
     """Directed graph."""
     def __init__(self, incoming_graph_data=None, **attr):
-        nx.DiGraph.__init__(self, incoming_graph_data, **attr)
         Graph.__init__(self)
+        nx.DiGraph.__init__(self, incoming_graph_data, **attr)
