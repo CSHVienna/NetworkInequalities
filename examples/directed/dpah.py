6afac39b9cdde38f1514000a783d4b2d101a5ef2
Jan Bachmann <jan@mannbach.de>
2024-08-08 11:23:47 +0200

    Provide asymmetric homophily

--- examples/directed/dpah.py
+++ examples/directed/dpah.py
@@ -1,5 +1,4 @@
-from netin import DPAH
-
+from netin.models import DPAHModel
 
 def run():
     n = 1000
@@ -7,13 +6,16 @@ def run():
     f_m = 0.1
     plo_M = 2.0
     plo_m = 2.0
-    h_MM = 0.5
+    h_MM = 0.2
     h_mm = 0.9
     seed = 1234
-    g = DPAH(n=n, d=d, f_m=f_m, plo_M=plo_M, plo_m=plo_m, h_MM=h_MM, h_mm=h_mm, seed=seed)
-    g.generate()
-    g.info()
-
+    m = DPAHModel(
+        N=n, d=d, f=f_m,
+        plo_M=plo_M, plo_m=plo_m,
+        h_m=h_mm, h_M=h_MM,
+        seed=seed)
+    m.simulate()
+    # g.info()
 
 if __name__ == '__main__':
     run()
--- examples/undirected/pah.py
+++ examples/undirected/pah.py
@@ -1,4 +1,4 @@
-from netin import PAH
+from netin.models import PAHModel
 
 
 def run():
@@ -8,10 +8,8 @@ def run():
     h_MM = 0.5
     h_mm = 0.5
     seed = 1234
-    g = PAH(n=n, k=k, f_m=f_m, h_MM=h_MM, h_mm=h_mm, seed=seed)
-    g.generate()
-    g.info()
-
+    m = PAHModel(N=n, m=k, f=f_m, h_m=h_mm, h_M=h_MM)
+    m.simulate()
 
 if __name__ == '__main__':
     run()
