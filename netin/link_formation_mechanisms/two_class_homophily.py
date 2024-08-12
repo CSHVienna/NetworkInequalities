a5bf0b38d8a496541a79616b68a65708e32b2ac8
Jan Bachmann <jan@mannbach.de>
2024-08-12 15:02:09 +0200

    Assert class number of super constructor

--- netin/link_formation_mechanisms/two_class_homophily.py
+++ netin/link_formation_mechanisms/two_class_homophily.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Union
 
 import numpy as np
 
@@ -6,6 +6,15 @@ from ..graphs.node_attributes import NodeAttributes
 from .homophily import Homophily
 
 class TwoClassHomophily(Homophily):
+    def __init__(
+            self,
+            node_class_values: NodeAttributes,
+            homophily: Union[float, np.ndarray], n_class_values: Union[int, None] = None) -> None:
+        super().__init__(node_class_values, homophily, n_class_values)
+        assert self.h.shape == (2, 2),\
+            ("Homophily matrix must be 2x2. Use "
+             "`Homophily` to define homophily for more than two classes.")
+
     @classmethod
     def from_two_class_homophily(
             cls, homophily: Tuple[float, float],
