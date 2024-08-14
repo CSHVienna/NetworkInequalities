639120e2f406fc185e71a880f3725b51b32a1702
Jan Bachmann <jan@mannbach.de>
2024-08-14 11:40:21 +0200

    Fix function call

--- netin/link_formation_mechanisms/link_formation_mechanism.py
+++ netin/link_formation_mechanisms/link_formation_mechanism.py
@@ -6,7 +6,7 @@ from ..graphs.node_vector import NodeVector
 class LinkFormationMechanism(BaseClass):
     def get_target_probabilities(self, source: int) -> NodeVector:
         probs = self._get_target_probabilities(source)
-        assert probs.sum == 1., "Probabilities do not sum to 1."
+        assert probs.sum() == 1., "Probabilities do not sum to 1."
         return probs
 
     @abstractmethod
