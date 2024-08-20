8d9a8fae05daf9b9768a2b88c79fda5ffd6765bb
Jan Bachmann <jan@mannbach.de>
2024-08-20 14:11:25 +0200

    Fix export of events

--- netin/graphs/__init__.py
+++ netin/graphs/__init__.py
@@ -1,6 +1,5 @@
 from .graph import Graph
 from .directed import DiGraph
-from ..event import Event
 from .node_vector import NodeVector
 from .categorical_node_vector import CategoricalNodeVector
 from .binary_class_node_vector import BinaryClassNodeVector
