d6c34356fc18927ab6c53d3a4d0e3e86763e2ad4
Jan Bachmann <jan@mannbach.de>
2024-08-20 14:28:07 +0200

    Fix parameters

--- netin/models/barabasi_albert_model.py
+++ netin/models/barabasi_albert_model.py
@@ -1,8 +1,9 @@
-from typing import Optional, Union
+from typing import Union
 
 import numpy as np
 
-from netin.graphs import Graph
+from netin.graphs.graph import Graph
+
 from .undirected_model import UndirectedModel
 from ..link_formation_mechanisms.preferential_attachment import PreferentialAttachment
 
@@ -11,12 +12,10 @@ class BarabasiAlbertModel(UndirectedModel):
             self, *args,
             N: int,
             m:int,
-            graph: Optional[Graph] = None,
             seed:  Union[int, np.random.Generator] = 1,
             **kwargs):
         super().__init__(
-            *args, N=N, m=m,
-            graph=graph, seed=seed, **kwargs)
+            *args, N=N, m=m, seed=seed, **kwargs)
         self.pa = PreferentialAttachment(N=N, graph=self.graph)
 
     def compute_target_probabilities(self, source: int) -> np.ndarray:
