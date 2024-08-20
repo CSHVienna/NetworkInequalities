958ada4a7d26f109db4b964f2f2c54f3f8b81908
Jan Bachmann <jan@mannbach.de>
2024-08-20 12:02:05 +0200

    Class to capture simulation time

---
+++ netin/utils/simulation_timer.py
@@ -0,0 +1,21 @@
+import time
+from typing import Optional
+
+from ..models.model import Model
+from ..event import Event
+
+class SimulationTimer:
+    _start_time: float
+    time: Optional[float] = None
+
+    def __init__(self, model: Model) -> None:
+        model.register_event_handler(
+            Event.SIMULATION_START, self._start_timer)
+        model.register_event_handler(
+            Event.SIMULATION_END, self._end_timer)
+
+    def _start_timer(self):
+        self._start_time = time.time()
+
+    def _end_timer(self):
+        self.time = time.time() - self._start_time
