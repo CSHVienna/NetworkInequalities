7233d3579e3eaa65ec1d28c9cc03207ab4447911
Jan Bachmann <jan@mannbach.de>
2024-08-21 15:45:55 +0200

    Set seed

--- examples/undirected/pah.py
+++ examples/undirected/pah.py
@@ -2,13 +2,13 @@ from netin.models import PAHModel
 
 
 def run():
-    n = 1000
+    n = 4000
     k = 2
     f_m = 0.1
     h_MM = 0.5
     h_mm = 0.5
     seed = 1234
-    m = PAHModel(N=n, m=k, f=f_m, h_m=h_mm, h_M=h_MM)
+    m = PAHModel(N=n, m=k, f_m=f_m, h_m=h_mm, h_M=h_MM, seed=seed)
     m.simulate()
 
 if __name__ == '__main__':
