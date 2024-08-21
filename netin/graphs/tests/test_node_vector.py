ead93b18ee36dde3202c3eb87fbd1c9365f7ccc1
Jan Bachmann <jan@mannbach.de>
2024-08-21 15:53:00 +0200

    Remove node label test

--- netin/graphs/tests/test_node_vector.py
+++ netin/graphs/tests/test_node_vector.py
@@ -19,20 +19,3 @@ class TestNodeVector:
         mask = node_values > 5
         assert np.sum(mask) == 4, "There must be 4 values greater than 5."
         assert np.all(node_values[mask] == np.arange(6, 10)), "Values must be 6, 7, 8, 9."
-
-    def test_custom_node_labels(self):
-        nodes = "abcde"
-        values = np.arange(len(nodes))
-        nv = NodeVector.from_ndarray(values, node_labels=list(nodes))
-
-        assert nv["a"] == 0, "Value for node `a` must be 0."
-        assert nv["e"] == 4, "Value for node `e` must be 4."
-        assert np.all(nv["a":"d"] == np.arange(0, 3)),\
-            "Values for nodes `a` to `d` must be 0 to 3."
-        with pytest.raises(KeyError):
-            _ = nv[0]
-        assert np.all(nv[np.asarray(["a", "c", "e"])] == np.array([0, 2, 4])),\
-            "Values for nodes np.array([`a`, `c`, `e`]) must be 0, 2, 4."
-        assert np.all(nv[["a", "c", "e"]] == np.array([0, 2, 4])),\
-            "Values for nodes `a`, `c`, `e` must be 0, 2, 4."
-
