4123d751512f0bbc6c589fb1d3b71e73aebbc973
Jan Bachmann <jan@mannbach.de>
2024-08-28 14:54:18 +0200

    Use timer in example

--- examples/undirected/pah.py
+++ examples/undirected/pah.py
@@ -1,5 +1,5 @@
 from netin.models import PAHModel
-
+from netin.utils import SimulationTimer
 
 def run():
     n = 4000
@@ -9,7 +9,9 @@ def run():
     h_mm = 0.5
     seed = 1234
     m = PAHModel(N=n, m=k, f_m=f_m, h_m=h_mm, h_M=h_MM, seed=seed)
+    timer = SimulationTimer(m)
     m.simulate()
+    print(f"Simulated model {m} in {timer.time:.2f} seconds.")
 
 if __name__ == '__main__':
     run()
