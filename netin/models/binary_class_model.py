315f55f181669f2b6eea92c5e1c9c1926695444a
Jan Bachmann <jan@mannbach.de>
2024-09-04 14:53:18 +0200

    Make use of BinaryClassGraphs

--- netin/models/binary_class_model.py
+++ netin/models/binary_class_model.py
@@ -5,9 +5,12 @@ import numpy as np
 from .model import Model
 from ..utils.constants import CLASS_ATTRIBUTE
 from ..graphs.binary_class_node_vector import BinaryClassNodeVector
+from ..graphs.binary_class_graph import\
+    BinaryClassGraph, BinaryClassDiGraph
 
 class BinaryClassModel(Model):
     f_m: float
+    graph: Union[BinaryClassGraph, BinaryClassDiGraph]
 
     def __init__(
             self, *args,
@@ -18,15 +21,23 @@ class BinaryClassModel(Model):
         self.f_m = f_m
 
     def _initialize_node_classes(self):
-        node_class_values = BinaryClassNodeVector.from_fraction(
-            N=self._n_nodes_total,
-            f_m=self.f_m,
-            rng=self._rng)
         if self.graph.has_node_class(CLASS_ATTRIBUTE):
             node_class_values_pre = self.graph.get_node_class(CLASS_ATTRIBUTE)
             assert isinstance(node_class_values_pre, BinaryClassNodeVector),\
             "The node class values must be binary"
-            node_class_values[:len(node_class_values_pre)] =\
+            if len(node_class_values_pre) < self._n_nodes_total:
+                ncv_post = BinaryClassNodeVector.from_fraction(
+                    N=self._n_nodes_total,
+                    f_m=self.f_m,
+                    rng=self._rng)
+                ncv_post[:len(node_class_values_pre)] =\
                 node_class_values_pre.values
-        self.graph.set_node_class(
-            CLASS_ATTRIBUTE, node_class_values)
+                self.graph.set_node_class(CLASS_ATTRIBUTE, ncv_post)
+        elif isinstance(self.graph, (BinaryClassGraph, BinaryClassDiGraph)):
+            self.graph.initialize_node_classes(
+                N=self._n_nodes_total,
+                f_m=self.f_m,
+                rng=self._rng)
+        else:
+            raise ValueError(
+                f"The graph must be a BinaryClassGraph or a BinaryClassDiGraph or have node classes `{CLASS_ATTRIBUTE}`")
