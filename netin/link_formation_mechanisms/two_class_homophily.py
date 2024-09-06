98397f31d70dd11f71475270a33fff8fb7a8516e
Jan Bachmann <jan@mannbach.de>
2024-09-06 15:50:33 +0200

    Add docs and change node class values type

--- netin/link_formation_mechanisms/two_class_homophily.py
+++ netin/link_formation_mechanisms/two_class_homophily.py
@@ -2,16 +2,28 @@ from typing import Tuple, Union
 
 import numpy as np
 
-from ..graphs.categorical_node_vector import CategoricalNodeVector
+from ..graphs.binary_class_node_vector import BinaryClassNodeVector
 from ..utils.validator import validate_float
 from .homophily import Homophily
 
 class TwoClassHomophily(Homophily):
+    """Two class homophily link formation mechanism.
+
+    This is a convenience class for the `Homophily` link formation mechanism with two classes.
+
+    Parameters
+    ----------
+    node_class_values : CategoricalNodeVector
+        The class assignment for each node (dimensions `n_nodes`).
+    homophily : Union[float, np.ndarray]
+        The homophily value(s).
+    """
     def __init__(
             self,
-            node_class_values: CategoricalNodeVector,
+            node_class_values: BinaryClassNodeVector,
             homophily: Union[float, np.ndarray]) -> None:
-        super().__init__(node_class_values=node_class_values, homophily=homophily)
+        super().__init__(
+            node_class_values=node_class_values, homophily=homophily)
         assert node_class_values.n_values == 2,\
             ("TwoClassHomophily can only be used for two classes. "
              f"Received {node_class_values.n_values} classes")
@@ -22,9 +34,10 @@ class TwoClassHomophily(Homophily):
     @classmethod
     def from_two_class_homophily(
             cls, homophily: Tuple[float, float],
-            node_class_values: CategoricalNodeVector)\
+            node_class_values: BinaryClassNodeVector)\
             -> "TwoClassHomophily":
-
+        """Creates a TwoClassHomophily instance from two homophily values.
+        """
         assert(len(homophily) == 2),\
             ("Homophily for two classes must be a tuple of two values. "
              f"Received {homophily}.")
