0da1125bfdff214266eab92494077497a04c3787
lisette-espin <lisette-espin@users.noreply.github.com>
2024-09-11 21:47:03 +0000

    deploy: 7af90f96d5d0da3b3e17036d053291984d8d0151

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0.0a1',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.0.8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
