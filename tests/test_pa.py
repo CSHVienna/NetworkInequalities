c8c02e9ea9cff8496f886b2da57dc78b0133ca28
FarzanaAkhi <farzana.akter@student.tugraz.at>
2025-12-04 23:05:57 +0100

    Default dependencies and optional dependencies: pyproject.toml

--- tests/test_pa.py
+++ tests/test_pa.py
@@ -1,19 +1,45 @@
 #import unittest 
-
+#Dependencies
 from netin.models import HomophilyModel, PAModel, PAHModel, PATCHModel, CompoundLFM
+from netin import viz
+from netin.stats import get_node_metadata_as_dataframe
 
 # Network parameters
 N = 1000
 m = 2
 f_m = 0.1
+h_MM = 0.9
+h_mm = 0.9
+tc = 0.8
 seed = 1234
 
 #**Generation** 
 
-# Generation: Preferential attachment
-m_pa = PAModel(n=N, k=m, f_m=f_m, seed=seed)
+# PA: Preferential attachment
+m_pa = PAModel(N=N, m=m, f_m=f_m, seed=seed)
 m_pa.simulate()
 
-###
-#m_pa.nodes[3]
-print(m_pa)
+# H: Homophily
+m_h = HomophilyModel(N=N, m=m, f_m=f_m, h_m=h_mm, h_MM=h_MM, seed=seed)
+m_h.simulate()
+
+# PAH: Preferential attachment and homophily
+m_pah = PAHModel(N=N, m=m, f_m=f_m, h_m=h_mm, h_MM=h_MM, seed=seed)
+m_pah.simulate()
+
+# PATCH: Preferential attachment, homophily, and triadic closure
+lfm_l = CompoundLFM.PAH
+lfm_g = CompoundLFM.PAH
+tau = .0
+
+m_patch = PATCHModel(N=N, m=m, f_m=f_m,
+                     tau=tau,
+                     lfm_tc=lfm_l, lfm_global=lfm_g,
+                     h_m=h_mm, h_M=h_MM,
+                     seed=seed)
+
+m_patch.simulate()
+
+#Plots
+viz.reset_style()
+viz.set_paper_style()
